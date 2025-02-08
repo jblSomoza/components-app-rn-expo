2dcc3bbf6671c5b26486aad85a7c55110223df82
jblSomoza <jhony-bryan@outlook.com>
2025-02-07 22:12:35 -0600

    Agregar componente ThemedButton y su uso en Animation101Screen

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,9 +1,23 @@
+import ThemedButton from '@/presentation/shared/ThemedButton';
 import { View, Text } from 'react-native';
 
 const Animation101Screen = () => {
   return (
     <View>
       <Text>Animation101Screen</Text>
+        <ThemedButton
+          onPress={() => {
+            console.log('Button pressed');
+          }}
+          className='mb-5'
+        >
+          Fade In
+        </ThemedButton>
+
+        <ThemedButton className='mb-5'>
+          Fade Out
+        </ThemedButton>
+
     </View>
   );
 };
---
+++ presentation/shared/ThemedButton.tsx
@@ -0,0 +1,20 @@
+import { View, Text, Pressable, PressableProps } from 'react-native'
+import React from 'react'
+
+interface ThemedButtonProps extends PressableProps{
+    className?: string;
+    children: string;
+}
+
+const ThemedButton = ({ children, className, ...rest }: ThemedButtonProps) => {
+  return (
+    <Pressable
+        className={`bg-light-primary dark:bg-dark-primary items-center rounded-xl px-6 py-2 active: opacity-80 ${className}`}
+        {...rest}
+    >
+        <Text className='text-white text-2xl'>{children}</Text>
+    </Pressable>
+  )
+}
+
+export default ThemedButton
