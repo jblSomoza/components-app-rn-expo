293be0f78a6dbc501ba0716d8a3d13696381f8ba
jblSomoza <jhony-bryan@outlook.com>
2025-02-07 22:20:49 -0600

    Agregar animaciones de desvanecimiento a Animation101Screen; implementar funciones fadeIn y fadeOut

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,24 +1,42 @@
-import ThemedButton from '@/presentation/shared/ThemedButton';
-import { View, Text } from 'react-native';
+import ThemedButton from "@/presentation/shared/ThemedButton";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { useRef } from "react";
+import { Animated, View } from "react-native";
 
 const Animation101Screen = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-        <ThemedButton
-          onPress={() => {
-            console.log('Button pressed');
-          }}
-          className='mb-5'
-        >
-          Fade In
-        </ThemedButton>
+    <ThemedView margin className="justify-center items-center flex-1">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
+        style={{ width: 150, height: 150, opacity: animatedOpacity }}
+      />
 
-        <ThemedButton className='mb-5'>
-          Fade Out
-        </ThemedButton>
+      <ThemedButton onPress={fadeIn} className="my-5 ">
+        Fade In
+      </ThemedButton>
 
-    </View>
+      <ThemedButton className="my-5" onPress={fadeOut}>
+        Fade Out
+      </ThemedButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
