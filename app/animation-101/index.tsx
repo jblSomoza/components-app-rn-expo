fd7bf4b3596c00069f50d9ccbece2e013aec0c40
jblSomoza <jhony-bryan@outlook.com>
2025-02-07 22:29:05 -0600

    Agregar animación de desplazamiento a Animation101Screen; implementar función de animación para la propiedad translateY

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,11 @@
 import ThemedButton from "@/presentation/shared/ThemedButton";
 import ThemedView from "@/presentation/shared/ThemedView";
 import { useRef } from "react";
-import { Animated, View } from "react-native";
+import { Animated, Easing, View } from "react-native";
 
 const Animation101Screen = () => {
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(animatedOpacity, {
@@ -12,21 +13,33 @@ const Animation101Screen = () => {
       duration: 1000,
       useNativeDriver: true,
     }).start();
+
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
   };
 
   const fadeOut = () => {
     Animated.timing(animatedOpacity, {
       toValue: 0,
-      duration: 1000,
+      duration: 500,
       useNativeDriver: true,
-    }).start();
+    }).start(() => animatedTop.setValue(-100));
   };
 
   return (
     <ThemedView margin className="justify-center items-center flex-1">
       <Animated.View
         className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
-        style={{ width: 150, height: 150, opacity: animatedOpacity }}
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity,
+          transform: [{ translateY: animatedTop }],
+        }}
       />
 
       <ThemedButton onPress={fadeIn} className="my-5 ">
