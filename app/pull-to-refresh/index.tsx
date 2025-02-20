c66610ff54d6b8079cb64c274182351729bb73d6
jblSomoza <jhony-bryan@outlook.com>
2025-02-19 22:17:44 -0600

    Agregar funcionalidad de pull-to-refresh; implementar ScrollView con RefreshControl y estilos temáticos

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
+import { View, Text, RefreshControl, ScrollView } from 'react-native';
+
 
 const PullToRefreshScreen = () => {
+
+  const primaryColor = useThemeColor({}, 'primary');
+  const backgroundColor = useThemeColor({
+    light: 'white',
+    dark: 'black',
+  }, 'background');
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setIsRefreshing(true);
+    // Simulate a network request
+    setTimeout(() => setIsRefreshing(false), 2000);
+  };
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          colors={[primaryColor, 'red', 'blue', 'green']}
+          title='Loading...'
+          progressBackgroundColor={backgroundColor}
+        />
+      }
+    >
+      <ThemedView margin>
+        <ThemedText type='h1'>PullToRefreshScreen</ThemedText>
+      </ThemedView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
